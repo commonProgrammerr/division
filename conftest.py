bb0d4dcebe88559731d4dca27a6376efcd59e8df
Andre Escorel <gustavo.escorel@gmail.com>
2025-02-19 20:26:55 -0300

    adding pytest

---
+++ conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from unittest.mock import patch
+from sqlmodel import create_engine
+from division import models
+
+
+@pytest.fixture(autouse=True)
+def go_to_tmpdir(request):
+    tmpdir = request.getfixturevalue("tmpdir")
+    with tmpdir.as_cwd():
+        yield
+
+
+@pytest.fixture(autouse=True, scope="function")
+def setup_testing_database(request):
+    """For each test, create a database file on tmpdir
+    force database.py to use that filepath.
+    """
+
+    if "no_test_db" in request.keywords:
+        yield
+        return
+
+    tmpdir = request.getfixturevalue("tmpdir")
+    test_db = str(tmpdir.join("database.test.db"))
+
+    engine = create_engine(f"sqlite:///{test_db}")
+    models.SQLModel.metadata.create_all(bind=engine)
+    with patch("division.database.engine", engine):
+        yield
